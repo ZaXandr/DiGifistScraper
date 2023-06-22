63125c1aaa4ef202e4b0fccb84ba3c242b2c8406
ZaXandr <Zakharov_Alex18@outlook.com>
2023-06-22 12:52:12 +0300

    add logs my PC (version 3.0.0)

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import json
 arguments = sys.argv
 
 json_str = arguments[1]
-#json_str = '{"them_name":["Sahara"],"category":["Health and beauty"],"clicks":["700"],"price":[],"catalog_size":[],"features":[],"them_id":["1006"]}'
+#json_str = '{"them_name":["Sahara"],"category":["Health and beauty"],"clicks":["700"],"price":[],"catalog_size":[],"features":[],"them_id":["1006"],"second_click":["True"]}'
 data = json.loads(json_str)
 
 search_text = data["them_name"][0]
@@ -32,7 +32,7 @@ clicks_per_hour = int(data["clicks"][0]) // 24
 second_click = bool(data["second_click"][0])
 
 proxy_list = []
-
+log = []
 API_KEY = "Token 50xn49ng5y38gkca2zdy8q0akqhi5gsp0sl7rsqo"
 
 i = 1
@@ -90,13 +90,12 @@ for i in range(clicks_per_hour):
         max_pages = 1
 
     conn = mysql.connector.connect(
-        host='localhost',
+        host='207.154.213.22',
         database='scraper',
         user='scraper',
         password='1234'
     )
     cursor = conn.cursor()
-    insert_query = "INSERT into output (name,position,them_id,set_amount,date) VALUES (%s,%s,%s,%s,%s)"
 
     for page_num in range(1, max_pages + 1):
         url_t = url_template.format(page_num=page_num)
@@ -108,18 +107,14 @@ for i in range(clicks_per_hour):
             if not any(element.text == e[0].text for e in unique_elements):
                 unique_elements.append((element, page_num))
 
+    them_name = ""
+    them_number = 0
+
     for i, element in enumerate(unique_elements):
         if search_text in element[0].text:
             page_num = str(element[1])
             them_number = i + 1
-            print(f"Element '{element[0].text}'. Counter: {them_number}")
-            current_time = datetime.now()
-            formatted_datetime = current_time.strftime('%Y-%m-%d %H:%M:%S')
-            data = (element[0].text, them_number, search_category_id, number_of_clicks, formatted_datetime)
-            cursor.execute(insert_query, data)
-            conn.commit()
-            cursor.close()
-            conn.close()
+            them_name = element[0].text
 
     current_proxy = random.choice(proxy_list)
     proxy_settings = f"{current_proxy['username']}:{current_proxy['password']}@{current_proxy['proxy_address']}:{current_proxy['port']}"
@@ -137,14 +132,15 @@ for i in range(clicks_per_hour):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+    chrome_options.add_argument("--lang=en")
 
     driver_path = "/usr/bin/chromedriver"
     service = Service(driver_path)
     driver = webdriver.Chrome(seleniumwire_options=proxy_options,options=chrome_options,service=service)
 
     driver.maximize_window()
-    print(url + "page=" + str(page_num) + "&sort_by=most_relevant")
     driver.get(url + "page=" + str(page_num) + "&sort_by=most_relevant")
+    log.append(driver.current_url)
     try:
         them_style = driver.find_element(By.XPATH, f"//a[@data-theme-name='{search_text}' and @data-state='0']")
         driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", them_style)
@@ -152,24 +148,40 @@ for i in range(clicks_per_hour):
         them_style.click()
         theme_to_click = driver.find_element(By.XPATH, f"//span[contains(text(), '{search_text}')]")
         theme_to_click.click()
+        log.append(driver.current_url)
         if second_click:
             view_to_click = driver.find_element(By.XPATH, "//a[contains(@class, 'marketing-button marketing-button--secondary theme-preview-link tw-text-body-md tw-link-base tw-link-inverted tw-link-md tw-border-t-0 tw-border-r-0 tw-border-l-0 tw-rounded-none tw-p-0 !tw-ml-0 hover:tw-bg-transparent')]")
             driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", view_to_click)
             time.sleep(2)
             view_to_click.click()
+            log.append(driver.current_url)
+            time.sleep(1)
     except NoSuchElementException:
         try:
             theme_to_click = driver.find_element(By.XPATH, f"//span[contains(text(), '{search_text}')]")
             driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", theme_to_click)
             time.sleep(1)
             theme_to_click.click()
+            time.sleep(1)
+            log.append(driver.current_url)
             if second_click:
                 view_to_click = driver.find_element(By.XPATH, "//a[contains(@class, 'marketing-button marketing-button--secondary theme-preview-link tw-text-body-md tw-link-base tw-link-inverted tw-link-md tw-border-t-0 tw-border-r-0 tw-border-l-0 tw-rounded-none tw-p-0 !tw-ml-0 hover:tw-bg-transparent')]")
                 driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", view_to_click)
                 time.sleep(2)
                 view_to_click.click()
+                log.append(driver.current_url)
+                time.sleep(1)
         except NoSuchElementException:
             print("element not found")
+    print(log)
 
+    insert_query = "INSERT into output (name,position,them_id,set_amount,date,log) VALUES (%s,%s,%s,%s,%s,%s)"
+    current_time = datetime.now()
+    formatted_datetime = current_time.strftime('%Y-%m-%d %H:%M:%S')
+    data = (them_name, them_number, search_category_id, number_of_clicks, formatted_datetime, str(log))
+    cursor.execute(insert_query, data)
+    conn.commit()
+    cursor.close()
+    conn.close()
     driver.quit()
     time.sleep(int(get_random_sleep(clicks_per_hour)))
